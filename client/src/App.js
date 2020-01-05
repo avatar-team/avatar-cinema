e1003464b6714b08070dace98fa5b269e50d4cd6
essam <esamfarg91@gmail.com>
2020-01-05 20:15:13 +0100

    prepare the components

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Link, Switch, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            good luck guys.
-          </p>
-            Learn React
-        </header>
-      </div>
-    </BrowserRouter>
-  );
+class App extends React.Component{
+  constructor() {
+    this.state = {}
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <p>
+              good luck guys.
+            </p>
+              Learn React
+          </header>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
