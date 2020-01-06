d396c2b47a458f267e3c7b66474d938afa7952ba
essam <esamfarg91@gmail.com>
2020-01-06 17:45:23 +0100

    most of functional component done

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,58 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Link, Switch, Route} from 'react-router-dom';
 import axios from 'axios';
+import movies from './components/dummyData';
+import NavBar from './components/NavBar'
 
 class App extends React.Component{
   constructor() {
     super()
     this.state = {
       movies: [],
-      currentReservation: {}
+      currentMovie: {}
     }
+    this.currentReservation = {sofian: 5}
   }
 
-  handleReservation(reservationData) {
-    axios.post("/api/reserveFilm", reservationData)
-    .then((res)=> {
-      this.setState({
-        currentReservation: res.data
-      })
-    })
-  }
 
   getMovies() {
     axios.get('/api/movies')
     .then((res)=> {
       console.log(res.data)
       this.setState({
-        movies: res.data
+        movies: movies
       })
     })
     .catch(err => {
       console.log(err)
     })
   }
+
+  handleReservation(reservationData) {
+    axios.post("/api/reserveFilm", reservationData)
+    .then((res)=> {
+      this.currentReservation =  res.data
+    })
+  }
+
+
+  handleSearch(videoTitle) {
+    this.state.movies.map((movie)=> {
+      if(movie.Title == videoTitle) {
+        this.setState({
+          currentMovie: movie
+        })
+      }
+
+    })
+  }
+
+  handleCardClick(i) {
+    this.setState({
+      currentMovie: this.state.movies[i]
+    })
+  }
+
 
   componentDidMount() {
     this.getMovies();
