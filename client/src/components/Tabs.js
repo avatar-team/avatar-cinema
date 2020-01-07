c6539d0752eb219255c0c0b8026fd5fad05d1cb1
alijalal-sys <ajmabilal@gmail.com>
2020-01-07 16:57:37 +0100

    Components Ready!

--- client/src/components/Tabs.js
+++ client/src/components/Tabs.js
@@ -2,16 +2,19 @@ import React from 'react'
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 
+const temp = {
+  width: '868px',
+  margin: '33px',
+  padding: '10px 10px 0px 10px'
+}
+
 const Daysbar = () => (
   <div>
-    <Tabs className="w-25 border border-top-0 mx-auto text-center" defaultActiveKey="home">
+    <Tabs style={temp} className="border text-center" defaultActiveKey="home">
       <Tab eventKey="Day1" title="5/1"></Tab>
       <Tab eventKey="Day2" title="6/1"></Tab>
       <Tab eventKey="Day3" title="7/1"></Tab>
       <Tab eventKey="Day4" title="8/1"></Tab>
-      <Tab eventKey="Day5" title="9/1"></Tab>
-      <Tab eventKey="Day6" title="10/1"></Tab>
-      <Tab eventKey="Day7" title="11/1"></Tab>
     </Tabs>
   </div>
 )
