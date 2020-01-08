696001df6ec1a4d24f1ac84e8546d4702b2df0c8
essam <esamfarg91@gmail.com>
2020-01-08 15:06:49 +0100

    fix conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MovieControlller from './components/MovieController.js'
 
 
 import  User from "./components/User.js"
