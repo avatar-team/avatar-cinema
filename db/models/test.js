577f4b17befc8bfe0ac43221cbf727e31f4d056c
alijalal-sys <ajmabilal@gmail.com>
2020-01-08 14:46:48 +0100

    User Routes Completed ,, Need some changing & refactoring

--- db/models/test.js
+++ db/models/test.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost/Avatar', (err) => {
 });
 
 const userModel = require('./userModel');
+const movieModel = require('./movieModel');
 
 // app.get('*', (req, res) => {
 //   res.send('Hello')
@@ -44,6 +45,45 @@ app.get('/api/users/:userName', (req, res, next) => {
   })
 })
 
+// Get Route 
+// To retrieve All users from DB
+app.get('/api/users', (req, res, next) => {
+  userModel.findUser((err, data) => {
+    if (err) res.status(404).send(err);
+    res.json({ data })
+  })
+})
+
+
+// Get Route 
+// it will recieve a username as params and it will find the Genre of movies
+// that he watched , and it will search inside DB finding the movies 
+// with the same Genre ,,
+// TODO: still need refactoring , and it depends on someone else work
+// so it is not finished yet 
+
+app.get('/api/users/:userName/recommendedMovies', (req, res, next) => {
+  userModel.findUser(req.params, (errUser, userData) => {
+    movieModel.findMovies({ Genre: userData.moviesBought[0].Genre }, (err, data) => {
+      res.json(data)
+    })
+  })
+})
+
+// res.json( userData.moviesBought[0].Genre )
+
+// userModel.findUser(req.params, (errUser, userData) => {
+//   if (errUser) res.status(404).send(errUser);
+//   movieModel.findMovies((errMovie, movieData) => {
+//     if (errMovie) res.status(404).send(errMovie);
+//     userModel.pushMoviesBought(userData._id, movieData._id, (errPush, result) => {
+//       if (errPush) res.send(errPush);
+//       res.json({ result })
+//     })
+//   })
+// })
+
+
 let user = {
   userName: 'Ali_Jalal',
   password: '00000000',
@@ -56,8 +96,64 @@ let user = {
 
 userModel.insertUser(user)
 
+let movie = {
+  Title: "John Wick",
+  Year: 2014,
+  Rated: "R",
+  Genre: "Action, Crime, Thriller",
+  Runtime: 123,
+  Plot:"An ex-hit-man comes out of retirement",
+  Poster: "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg",
+  imdbRating: "7.4",
+  availability:true,
+  price:16,
+  availableChairs:39,
+  playDate: new Date(2020,6,24,12,30,20),
+  movieTrailer:"aojfdbiayhg",
+  chairs: 16
+}
+
+let movie2 = {
+  Title: "John Wick 2",
+  Year: 2014,
+  Rated: "R",
+  Genre: "Action, Crime, Thriller",
+  Runtime: 123,
+  Plot:"An ex-hit-man comes out of retirement",
+  Poster: "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg",
+  imdbRating: "7.4",
+  availability:true,
+  price:16,
+  availableChairs:39,
+  playDate: new Date(2020,6,24,12,30,20),
+  movieTrailer:"aojfdbiayhg",
+  chairs: 16
+}
+
+let movie3 = {
+  Title: "John Wick 3",
+  Year: 2019,
+  Rated: "R",
+  Genre: "Action, Crime, Thriller",
+  Runtime: 123,
+  Plot:"An ex-hit-man comes out of retirement",
+  Poster: "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg",
+  imdbRating: "7.4",
+  availability:true,
+  price:16,
+  availableChairs:32,
+  playDate: new Date(2020,6,24,12,30,20),
+  movieTrailer:"aojfdbiayhg",
+  chairs: 16
+}
+
+// movieModel.insertMovie(movie)
+// movieModel.insertMovie(movie2)
+// movieModel.insertMovie(movie3)
 
 
 
 
 app.listen(8000)
+
+const movieDb = require('./movieModel');
