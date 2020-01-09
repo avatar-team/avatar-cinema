b3f00205befea4ce347972f8ee3cf06de026e842
BigBBaker <abobkerelaghel@gmail.com>
2020-01-09 11:38:45 +0100

    added login-sginup routes

--- serverOptimazeAbobker/server.js
+++ serverOptimazeAbobker/server.js
@@ -8,7 +8,7 @@ const axios = require('axios'); //might have to move them in the movieController
 const movieDb = require('./db/models/movieModel'); //might have to move them in the movieController
 const reservationDb = require('./db/models/reservationModel'); //might have to move them in the movieController
 const adminDb = require('./db/models/adminModel');
-
+const authController = require('../../controllers/authController')
 
 const movieRoute = require('./routes/movieRoute')
 const userRoute = require('./routes/userRoute')
